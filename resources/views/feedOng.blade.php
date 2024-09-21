00bc794493812e154492056e77b0c56000887183
BeuEni <isabelenicavalcantedasilva@gmail.com>
2024-09-21 20:44:27 -0300

    ong

--- resources/views/feedOng.blade.php
+++ resources/views/feedOng.blade.php
@@ -43,49 +43,5 @@
 
         </div><!--nav-->
 
-        <div class="container-feed">
-
-        <div class="perfil-postagem">
-
-            <div class="foto-postagem">
-            <i class="bi bi-person-circle"></i>
-            </div>
-
-            <div class="nomeOng-postagem">Ong_Exemplo__</div>
-
-            <div class="maisOpcoes-postagem">
-            <i class="bi bi-three-dots-vertical"></i>
-            </div>
-        </div><!--perfil-->
-
-        <div class="postagem">
-
-        <div class="foto-anexo">
-                <img src="/img/exemplo.jpg" alt="">
-            </div>
-
-            <div class="icons">
-                <div class="curtida"></div>
-                <div class="comentarios-icon"></div>
-            </div>
-
-            <div class="descricao-postagem">
-                <div class="nomeOng-postagem">Ong_Exemplo__</div>
-                <div class="descricao-text">Hoje recebemos carregamento de cestas básicas.</div>
-            </div>
-
-            <div class="comentarios">
-                <p>Adicionar um comentário...</p>
-            </div>
-
-            <div class="momento-postagem">
-                <p>Postado há 21 horas</p>
-            </div>
-
-        </div><!--postagem-->
-            
-        </div><!--container-feed-->
-
-    
 </body>
 </html>
