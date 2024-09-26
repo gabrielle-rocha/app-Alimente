461b27cea4df5b9e6c402fe6705078bb6fdffd51
Yasmin <targetsoftware1@gmail.com>
2024-09-26 10:31:54 -0300

    yasmin

--- resources/views/editarperfildoador.blade.php
+++ resources/views/editarperfildoador.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil do Doador</title>
     <style>
+        
         * {
             margin: 0;
             padding: 0;
